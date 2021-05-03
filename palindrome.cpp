0f18b884367ce3ca655d841562182c9435f05474
Harshith69 <68234861+Harshith69@users.noreply.github.com>
2021-05-03 21:55:00 +0530

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, reversedNumber = 0, remainder;
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    while(n != 0) {
+        remainder = n%10;
+        reversedNumber = reversedNumber*10 + remainder;
+        n /= 10;
+    }
+    cout << "Reversed Number = " << reversedNumber;
+    return 0;
+}
